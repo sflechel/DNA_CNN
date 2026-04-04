efe66af77820121b3536849dcbb273ddd7e39afc
sflechel <solalflechelles@gmail.com>
2026-04-04 17:28:59 +0200

    put data in torch class for training

--- src/load_data.py
+++ src/load_data.py
@@ -1,7 +1,45 @@
 import pandas as pd
 import pysam
-
+import torch
+from torch.utils.data import Dataset
+from torch import Tensor
 from dna_utils import one_hot_encode
+
+
+class DNASeqDataset(Dataset):
+    def __init__(self, bed_file: str, fasta_file: str, half_window=500):
+        self.half_window = half_window
+        self.fasta_file = fasta_file
+        cols = [
+            "chrom",
+            "start",
+            "end",
+            "name",
+            "score",
+            "strand",
+            "sig",
+            "p",
+            "q",
+            "peak",
+        ]
+        all_peaks = pd.read_csv(bed_file, sep="\t", names=cols)
+        self.peaks = all_peaks[all_peaks["chrom"] == "chr22"]
+        self.genome = None
+
+    def __len__(self):
+        return len(self.peaks)
+
+    def __getitem__(self, index) -> tuple[Tensor, Tensor]:
+        peak = self.peaks.iloc[index]
+        if self.genome is None:
+            self.genome = pysam.FastaFile(self.fasta_file)
+        seq = load_seq_at_peak(
+            genome, peak["chrom"], peak["start"], peak["end"], self.half_window
+        )
+        encoded = one_hot_encode(seq)
+        return torch.tensor(encoded, dtype=torch.float32), torch.tensor(
+            1.0, dtype=torch.float32
+        )  # we also return the label, set to true
 
 
 def load_seq_at_peak(
@@ -12,7 +50,10 @@ def load_seq_at_peak(
     if newStart < 0:
         start = 0
         end = 1000
-    return genome.fetch(chr, newStart, end)
+    seq = genome.fetch(chr, newStart, end)
+    if len(seq) < half_window * 2:
+        seq = seq + seq.ljust(half_window * 2, "N")
+    return seq
 
 
 if __name__ == "__main__":
